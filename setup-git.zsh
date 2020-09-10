5c6a86fe6d0c898652b9dc921858b569a7649ca6
Jeremias Erbs <jerry@badabam.de>
2020-09-10 22:32:38 +0200

    Update git name wording

--- setup-git.zsh
+++ setup-git.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
-vared -p 'Enter your full name (first and lastname): ' -c fullname &&
+vared -p 'Enter your full name (first lastname): ' -c fullname &&
 git config --global user.name $fullname && 
 vared -p 'Enter your e-mail (you@yourdomain.com): ' -c email &&
 git config --global user.email $email;
