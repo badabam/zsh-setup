779c3639d96795f079e686eff7727d4e58a0ae19
Jeremias Erbs <jerry@badabam.de>
2020-09-10 21:32:09 +0200

    Add setup-git.zsh

---
+++ setup-git.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+vared -p 'Enter your full name (first and lastname): ' -c fullname &&
+git config --global user.name $fullname && 
+vared -p 'Enter your e-mail (you@yourdomain.com): ' -c email &&
+git config --global user.email $email;
+git config --global pull.ff "only"
+git config --global core.pager cat
